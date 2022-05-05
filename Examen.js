2e660ab93e9365cd384a9bb25ab02484a17d92bb
Yuriana Jimenez <103084843+YuriGGG5@users.noreply.github.com>
2022-05-04 20:39:32 -0500

    Add files via upload

---
+++ Examen.js
@@ -0,0 +1,27 @@
+function reporteDePasajeros (numeroDeEstaciones) {
+    let acumulador =200; 
+    for (let i=0; i<numeroDeEstaciones; i++) { if (numeroDeEstaciones>0){acumulador=acumulador+15-30}else if (numeroDeEstaciones===5){acumulador=acumulador+120-80}else{acumulador=acumulador+15-30}
+    return ("En la estación "+ i + "hay "+ acumulador + "pasajeros arriba del tren")
+    } 
+    }
+
+ 
+function asientosDisponibles (conjuntoDeAsientosDisponibles, asientoQueQuiereOcuparLaPErsona) 
+
+{ if(conjuntoDeAsientosDisponibles[]=== conjuntoDeAsientosDisponibles){ return 
+("Felicitaciones, el asiento número " asientoQueQueiereOcuparLaPersona " está disponible")
+}
+else{
+conjuntoDeAsientosDisponibles[] !== conjuntoDeAsientosDisponibles
+"Lo sentimos, el asiento número " asientoQueQuiereOcuparLaPersona "está ocupado, pero aún quedan " conjuntoDeAsientosDisponibles.length " asientos disponibles"}
+}
+
+function sumatoriaBajoImporte (importes) {let acumulador =0; for(let i=0; i<importes;i++){
+    if (importes[]>1.000) {
+    acumulador= acumulador + i
+    }else {acumulador=acumulador}
+    
+    }
+    return acumulador
+    
+    }
